e4aabdb85b9a2e828c87beb495a8bccca7f75a87
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-05-24 02:02:59 +0300

    chore: set higher resolution image for large background

--- src/components/FullSizePageWithAlbumCard/ImageBackground/index.js
+++ src/components/FullSizePageWithAlbumCard/ImageBackground/index.js
@@ -2,11 +2,12 @@
 import React, { forwardRef } from 'react';
 
 import './image-background.css';
+import { getNewUrlWithNewArtworkImageRes } from 'utils/helpers';
 
 const ImageBackground = forwardRef(({ url }, ref) => (
   <div
     ref={ref}
-    style={{backgroundImage: `url("${url}")`}}
+    style={{backgroundImage: `url("${getNewUrlWithNewArtworkImageRes(url, 800)}")`}}
     className='image-background fade' />
 ));
 
